d0c0c41e7e55cf3e8ad62c4e15cd21da12d4c139
senzelden <sunless@gmx.net>
2020-08-09 23:29:29 +0200

    Add primary keys and respective ids to countries and cities table

--- cities_geodata_import.sql
+++ cities_geodata_import.sql
@@ -1,9 +1,48 @@
-CREATE TABLE cities_geodata (
-    city_id INTEGER PRIMARY KEY,
-    city_name VARCHAR(100),
-    lat REAL,
-    lon REAL,
-    distance_to_klagenfurt REAL
+-- CREATE TABLE cities_geodata (
+--     city_id INTEGER PRIMARY KEY,
+--     city_name VARCHAR(100),
+--     lat REAL,
+--     lon REAL,
+--     distance_to_klagenfurt REAL
+-- );
+--
+-- \COPY cities_geodata FROM '/home/denniss/Desktop/Coding/spiced/bachmannpreis/cities_geodata.csv' DELIMITER ',' CSV HEADER;
+--
+--
+-- CREATE TABLE authors_cities (
+--     text_city_mapper_id INTEGER PRIMARY KEY,
+--     text_id INTEGER,
+--     city_name VARCHAR(200),
+--     city_id INTEGER,
+--
+--     FOREIGN KEY (text_id)
+--         REFERENCES texts,
+--     FOREIGN KEY (city_id)
+--         REFERENCES cities_geodata
+-- );
+--
+-- \COPY authors_cities FROM '/home/denniss/Desktop/Coding/spiced/bachmannpreis/cities_updated.csv' DELIMITER ',' CSV HEADER;
+
+
+-- CREATE TABLE countries (
+--     country_id INTEGER PRIMARY KEY,
+--     country_name VARCHAR(200)
+-- );
+--
+-- \COPY countries FROM '/home/denniss/Desktop/Coding/spiced/bachmannpreis/unique_countries.csv' DELIMITER ',' CSV HEADER;
+
+
+CREATE TABLE authors_countries (
+    text_country_mapper_id INTEGER PRIMARY KEY,
+    text_id INTEGER,
+    author_country VARCHAR(200),
+    country_detail VARCHAR(100),
+    country_id INTEGER,
+
+    FOREIGN KEY (text_id)
+        REFERENCES texts,
+    FOREIGN KEY (country_id)
+        REFERENCES countries
 );
 
-\COPY cities_geodata FROM '/home/denniss/Desktop/Coding/spiced/bachmannpreis/cities_geodata.csv' DELIMITER ',' CSV HEADER;
+\COPY authors_countries FROM '/home/denniss/Desktop/Coding/spiced/bachmannpreis/countries_updated.csv' DELIMITER ',' CSV HEADER;
