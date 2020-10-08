692d81edc939363de22f310537f651b7b73ebaa7
Dennis Senzel <sunless@gmx.net>
2020-10-08 23:08:41 +0200

    add pickled autosklearn model

--- prediction/prediction_auto-sklearn.py
+++ prediction/prediction_auto-sklearn.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import autosklearn.classification
 import sklearn.metrics
+from joblib import load
 
 df = pd.read_csv('../data/database_csvs/prediction_table.csv')
 
@@ -14,17 +15,19 @@ y_test = df[df.year == 2020]["price_won"]
 # rf_model.fit(X_train, y_train)
 # print(rf_model.score(X_test, y_test))
 
-automl = autosklearn.classification.AutoSklearnClassifier(
-    include_estimators=["random_forest",],
-    exclude_estimators=None,
-    include_preprocessors=["no_preprocessing",],
-    exclude_preprocessors=None,
-)
+# automl = autosklearn.classification.AutoSklearnClassifier(
+#     include_estimators=["random_forest",],
+#     exclude_estimators=None,
+#     include_preprocessors=["no_preprocessing",],
+#     exclude_preprocessors=None,
+# )
+#
+# automl.fit(X_train, y_train)
 
-automl.fit(X_train, y_train)
+automl = load('bachmann_automl.joblib')
 
 predictions = automl.predict(X_test)
-
+print(predictions)
 print(automl.sprint_statistics())
 print("Accuracy score:{}".format(
     sklearn.metrics.accuracy_score(y_test, predictions))
